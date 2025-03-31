d22a15b57be1e4035c92b57053b49eea7bb166ac
KimTaeEun1632 <qpwoal1324@naver.com>
2025-03-31 19:16:41 +0900

    feat: sideBar 추가(기능x)

---
+++ src/components/Common/SideBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SideBar = () => {
+  return <div className="text-white">내 라이브러리</div>;
+};
+
+export default SideBar;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,67 @@
+import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
-import React from "react";
-import arrow from "@/assets/images/arrow.svg";
 import Image from "next/image";
+import React, { useState } from "react";
+import arrow from "@/assets/images/arrow.svg";
+import SideBar from "../Common/SideBar";
 
 const Nav = () => {
+  const [isOpenSideBar, setIsOpenSideBar] = useState(false);
+
+  const handOpenSideBar = (event) => {
+    event.preventDefault();
+    setIsOpenSideBar(!isOpenSideBar);
+  };
+
+  const handleCloseSideBar = () => {
+    setIsOpenSideBar(false);
+  };
+
   return (
-    <header className="flex items-center fixed z-10 text-white top-0 w-full h-20">
-      <div className="flex justify-between items-center pl-10 pr-10 w-full h-full ">
-        <div className="flex justify-center items-center">
-          <Link className="text-2xl" href="/">
-            NailArtX
-          </Link>
-          <button>
-            <Image
-              className="text-white"
-              src={arrow}
-              alt="아래방향 화살표"
-              width={28}
-              height={28}
-            />
-          </button>
+    <>
+      <header className="flex items-center fixed z-100 text-white top-0 w-full h-20">
+        <div className="flex justify-between items-center pl-10 pr-10 w-full h-full">
+          <div className="relative flex justify-center items-center">
+            <Link className="text-2xl" href="/">
+              NailArtX
+            </Link>
+            <button onClick={handOpenSideBar}>
+              <Image
+                src={arrow}
+                alt="아래방향 화살표"
+                width={28}
+                height={28}
+                className={`text-white transition-transform ${
+                  isOpenSideBar ? "rotate-180" : "rotate-0"
+                }`}
+              />
+            </button>
+          </div>
+          <div>프로필</div>
         </div>
-        <div>프로필</div>
-      </div>
-    </header>
+      </header>
+
+      {/* 사이드바 */}
+      <AnimatePresence>
+        {isOpenSideBar && (
+          <div className="fixed inset-0 z-[90]">
+            <div
+              className="absolute inset-0 bg-black/50 backdrop-blur-sm"
+              onClick={handleCloseSideBar}
+            />
+            <motion.div
+              initial={{ y: -50, opacity: 0 }} // 위에서 시작
+              animate={{ y: 0, opacity: 1 }} // 아래로 이동하며 나타남
+              exit={{ y: -50, opacity: 0 }} // 위로 사라짐
+              transition={{ duration: 0.1 }}
+              className="absolute top-20 left-10"
+            >
+              <SideBar />
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
