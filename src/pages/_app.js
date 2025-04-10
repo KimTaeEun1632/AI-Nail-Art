7f8320515fe8dd63f8a069a89b89bfb711559626
KimTaeEun1632 <qpwoal1324@naver.com>
2025-04-10 19:02:55 +0900

    feat: 회원가입 로직 추가(스타일 x)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,23 @@
 import MainLayout from "@/components/Layout/MainLayout";
 import { ImagesProvider } from "@/lib/ImagesContext";
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const queryClient = new QueryClient();
   const getLayout =
     Component.getLayout ?? ((page) => <MainLayout>{page}</MainLayout>);
   return (
-    <SessionProvider session={session}>
-      <ImagesProvider>{getLayout(<Component {...pageProps} />)}</ImagesProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <ImagesProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </ImagesProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
--- src/pages/auth/signUp.jsx
+++ src/pages/auth/signUp.jsx
@@ -1,5 +1,11 @@
+import { auth } from "@/apis/auth/auth";
+import SignInput from "@/components/Common/SignInput";
+import EmptyLayout from "@/components/Layout/EmptyLayout";
 import { USER_INPUT_VALIDATION } from "@/constants/user";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const { email, password, passwordConfirm, nickname } = USER_INPUT_VALIDATION;
 
@@ -34,8 +40,85 @@ const rules = {
     },
   },
 };
-const signUp = () => {
-  return <div>회원가입 페이지</div>;
+const signup = () => {
+  const { formState, register, handleSubmit, getValues } = useForm({
+    mode: "onBlur",
+  });
+
+  const router = useRouter();
+
+  const signMutation = useMutation({
+    mutationFn: (data) => auth.signUp(data),
+    mutationKey: ["signUp"],
+    onSuccess: () => {
+      router.push("/auth/signin");
+    },
+    onError: (error) => {
+      alert(error.response?.data.message);
+    },
+  });
+
+  const { isValid, errors } = formState;
+
+  const onsubmit = (data) => {
+    signMutation.mutate(data);
+  };
+
+  return (
+    <div>
+      회원가입 페이지
+      <form onSubmit={handleSubmit(onsubmit)}>
+        <SignInput
+          label="이메일"
+          type="email"
+          placeholder="이메일을 입력해 주세요"
+          isError={errors.email}
+          errorMessage={errors.email?.message}
+          {...register("email", rules.emailRules)}
+        />
+        <SignInput
+          label="닉네임"
+          type="text"
+          placeholder="닉네임을 입력해 주세요"
+          isError={errors.nickname}
+          errorMessage={errors.nickname?.message}
+          {...register("nickname", rules.nicknameRules)}
+        />
+        <SignInput
+          label="비밀번호"
+          type="password"
+          placeholder="비밀번호를 입력해 주세요"
+          isError={errors.password}
+          errorMessage={errors.password?.message}
+          {...register("password", rules.passwordRules)}
+        />
+        <SignInput
+          label="비밀번호 확인"
+          type="password"
+          placeholder="비밀번호를 한번 더 입력해 주세요"
+          isError={errors.passwordConfirm}
+          errorMessage={errors.passwordConfirm?.message}
+          {...register("passwordConfirm", {
+            validate: {
+              notMatch: (value) => {
+                const { password } = getValues();
+                return (
+                  password === value || passwordConfirm?.errorMessage.confirm
+                );
+              },
+            },
+          })}
+        />
+        <button type="submit" disabled={!isValid}>
+          회원가입
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default signUp;
+export default signup;
+
+signup.getLayout = function getLayout(page) {
+  return <EmptyLayout>{page}</EmptyLayout>;
+};
